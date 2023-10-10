0ab422b5c378af20d7be824f9dd57eb7222720c8
DlamondFox <gds200508@gmail.com>
2023-10-10 21:32:54 +0300

    Modified main.js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 
 fs.readFile('data.json', (err, data) => {
     const jsonData = JSON.parse(data);
-    const formatData = jsonData.map(item => {
-        return `${item.exchangedate}:${item.rate}`;
-    }).join('\n');
+    const formatData = jsonData.map(item => `${item.exchangedate}:${item.rate}`).join('\n');
     fs.writeFile('output.txt', formatData, (err) => {});
 });
